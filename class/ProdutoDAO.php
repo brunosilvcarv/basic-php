15b9dff27aed180116bddc71bdd9cede989a3957
brunosilvcarv <brunosilvcarv@gmail.com>
2018-09-09 03:47:32 -0400

    aplicando herança

--- class/ProdutoDAO.php
+++ class/ProdutoDAO.php
@@ -22,14 +22,21 @@ class ProdutoDAO {
   		$categoria = new Categoria();
   		$categoria->setNome($produto_array['categoria_nome']);
 
+      $produto_id = $produto_array['id'];
   		$nome = $produto_array['nome'];
   		$descricao = $produto_array['descricao'];
   		$preco = $produto_array['preco'];
   		$usado = $produto_array['usado'];
+      $isbn = $produto_array['isbn'];
+      $tipoProduto = $produto_array['tipoProduto'];
+      if($tipoProduto == "Livro") {
+      	$produto = new Livro($nome, $preco, $descricao, $categoria, $usado);
+      	$produto->setIsbn($isbn);
+      } else {
+      	$produto = new Produto($nome, $preco, $descricao, $categoria, $usado);
+      }
 
-  		$produto = new Produto($nome, $preco, $descricao, $categoria, $usado);
-  		$produto->setId($produto_array['id']);
-
+      $produto->setId($produto_id);
   		array_push($produtos, $produto);
   	}
 
@@ -38,26 +45,37 @@ class ProdutoDAO {
 
   function insereProduto(Produto $produto) {
 
-  	$query = "insert into produtos (nome, preco, descricao, categoria_id, usado)
+    $isbn = "";
+    if($produto->temIsbn()) {
+      $isbn = $produto->getIsbn();
+    }
+    $tipoProduto = get_class($produto);
+  	$query = "insert into produtos (nome, preco, descricao, categoria_id, usado, isbn, tipoProduto)
   		values ('{$produto->getNome()}', {$produto->getPreco()},
   			'{$produto->getDescricao()}', {$produto->getCategoria()->getId()},
-  				{$produto->isUsado()})";
+  				{$produto->isUsado()}, '{$isbn}', '{$tipoProduto}')";
 
   	return mysqli_query($this->conexao, $query);
   }
 
   function alteraProduto(Produto $produto) {
 
+    $isbn = "";
+    if($produto->temIsbn()) {
+      $isbn = $produto->getIsbn();
+    }
+    $tipoProduto = get_class($produto);
   	$query = "update produtos set nome = '{$produto->getNome()}',
   		preco = {$produto->getPreco()}, descricao = '{$produto->getDescricao()}',
   			categoria_id= {$produto->getCategoria()->getId()},
-  				usado = {$produto->isUsado()} where id = '{$produto->getId()}'";
+  				usado = {$produto->isUsado()}, isbn = '{$isbn}', tipoProduto = '{$tipoProduto}' where id = '{$produto->getId()}'";
 
   	return mysqli_query($this->conexao, $query);
   }
 
   function buscaProduto($id) {
 
+    $id = $produto->getId();
   	$query = "select * from produtos where id = {$id}";
   	$resultado = mysqli_query($this->conexao, $query);
   	$produto_buscado = mysqli_fetch_assoc($resultado);
@@ -69,8 +87,16 @@ class ProdutoDAO {
   	$descricao = $produto_buscado['descricao'];
   	$preco = $produto_buscado['preco'];
   	$usado = $produto_buscado['usado'];
+    $isbn = $produto_buscado['isbn'];
+    $tipoProduto = $produto_buscado['tipoProduto'];
 
-  	$produto = new Produto($nome, $preco, $descricao, $categoria, $usado);
+    if($tipoProduto == "Livro") {
+      $produto = new Livro($nome, $preco, $descricao, $categoria, $usado);
+      $produto->setIsbn($isbn);
+    } else {
+      $produto = new Livro($nome, $preco, $descricao, $categoria, $usado);
+    }
+
   	$produto->setId($produto_buscado['id']);
 
   	return $produto;
--- produto-formulario-base.php
+++ produto-formulario-base.php
@@ -1,14 +1,14 @@
 <tr>
 	<td>Nome</td>
 	<td>
-		<input class="form-control" type="text" name="nome" 
+		<input class="form-control" type="text" name="nome"
 			value="<?=$produto->getNome()?>">
 	</td>
 </tr>
 <tr>
 	<td>Preço</td>
 	<td>
-		<input class="form-control" type="number" step="0.01" name="preco" 
+		<input class="form-control" type="number" step="0.01" name="preco"
 			value="<?=$produto->getPreco()?>">
 	</td>
 </tr>
@@ -27,16 +27,46 @@
 	<td>
 		<select name="categoria_id" class="form-control">
 			<?php
-			foreach($categorias as $categoria) : 
+			foreach($categorias as $categoria) :
 				$essaEhACategoria = $produto->getCategoria()->getId() == $categoria->getId();
 				$selecao = $essaEhACategoria ? "selected='selected'" : "";
 			?>
 				<option value="<?=$categoria->getId()?>" <?=$selecao?>>
 					<?=$categoria->getNome()?>
 				</option>
-			<?php 
+			<?php
 			endforeach
 			?>
 		</select>
+	</td>
+</tr>
+<tr>
+	<td>Tipo Produto</td>
+	<td>
+		<select name="tipoProduto" class="form-control">
+			<?php
+			$tipos = array("Produto","Livro");
+			foreach($tipos as $tipo) :
+				$esseEhOTipo = get_class($produto) == $tipo;
+				$selecao = $esseEhOTipo ? "selected='selected'" : "";
+			?>
+				<option value="<?=$tipo?>" <?=$selecao?>>
+					<?=$tipo ?>
+				</option>
+			<?php
+			endforeach
+			?>
+		</select>
+	</td>
+</tr>
+<tr>
+	<td>ISBN (caso seja um Livro)</td>
+	<td>
+		<input  class="form-control" type="text" name="isbn" value="
+		<?php
+		if($produto->temIsbn()) {
+				echo $produto->getIsbn();
+		}
+		?>">
 	</td>
 </tr>
