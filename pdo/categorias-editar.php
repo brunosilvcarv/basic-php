4861ee2c52d8a9de0f2d3fbeaa1283a0c330bdeb
brunosilvcarv <brunosilvcarv@gmail.com>
2018-09-20 16:23:26 -0400

    carregando a categoria

--- pdo/categorias-editar.php
+++ pdo/categorias-editar.php
@@ -1,3 +1,9 @@
+<?php require_once('classes/Categoria.php'); ?>
+<?php
+  $id = $_GET['id'];
+  $categoria = new Categoria($id);
+
+?>
 <?php require_once 'cabecalho.php' ?>
 <div class="row">
     <div class="col-md-12">
@@ -9,7 +15,7 @@
         <div class="col-md-6 col-md-offset-3">
             <div class="form-group">
                 <label for="nome">Nome da Categoria</label>
-                <input type="text" value="Livros" class="form-control" placeholder="Nome da Categoria">
+                <input type="text" value="<?php echo $categoria->nome ?>" class="form-control" placeholder="Nome da Categoria">
             </div>
             <input type="submit" class="btn btn-success btn-block" value="Salvar">
         </div>
--- pdo/classes/Categoria.php
+++ pdo/classes/Categoria.php
@@ -6,6 +6,13 @@ class Categoria {
     public $id;
     public $nome;
 
+    public function __construct($id = false) {
+      if($id) {
+        $this->id = $id;
+        $this->carregar();
+      }
+    }
+
     public function listar() {
         $query = "SELECT id, nome FROM categorias";
         $conexao = Conexao::getConexao();
@@ -14,8 +21,26 @@ class Categoria {
         return $lista;
     }
 
+    public function carregar() {
+      $query = "select id, nome from categorias where id =" . $this->id;
+      $conexao = Conexao::getConexao();
+      // exec()   ----> inserts, updates e deletes
+      // query()  ----> selects
+      $resultado = $conexao->query($query);
+      $lista = $resultado->fetchAll();
+      foreach ($lista as $linha) {
+        $this->nome = $linha['nome'];
+      }
+    }
+
     public function inserir() {
       $query = "insert into categorias(nome) values ('" . $this->nome . "')";
+      $conexao = Conexao::getConexao();
+      $conexao->exec($query);
+    }
+
+    public function atualizar() {
+      $query = "update categorias set nome = '" . $this->nome . "' where id = " . $this->id;
       $conexao = Conexao::getConexao();
       $conexao->exec($query);
     }
